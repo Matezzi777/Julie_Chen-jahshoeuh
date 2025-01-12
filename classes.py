e26f5cb5a17a5cda6379bd94c2fe142dda7f0891
Matezzi777 <matezzi777@gmail.com>
2025-01-12 19:47:18 +0100

    tag ad removed from embed footer

--- classes.py
+++ classes.py
@@ -5,7 +5,7 @@ from database import add_confessional, remove_confessional
 
 class Bot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix="!", intents=discord.Intents.all(), description=f"Julie Chen v{VERSION} by @matezzi75.", help_command=None)
+        super().__init__(command_prefix="%", intents=discord.Intents.all(), description=f"Julie Chen v{VERSION} by @matezzi75. Send me a DM for your own custom bot.", help_command=None)
 
 #==================== COLOURS & INFOS ===================
 BOT_EMBED_RGB = discord.Colour.from_rgb(172, 59, 241)
@@ -21,7 +21,7 @@ class BotEmbed(discord.Embed):
             url=url,
             description=description,
             timestamp=timestamp)
-        self.set_footer(text=f"Julie Chen v{VERSION} by @matezzi75", icon_url="https://avatars.githubusercontent.com/u/146573782?s=400&u=a8b68f52dded16bbbb8b9dcce5004aff0a092b8c&v=4")
+        self.set_footer(text=f"Julie Chen v{VERSION}")
 
 #========================= VIEW =========================
 class SetConfView(discord.ui.View):
